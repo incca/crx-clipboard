85ed8002a3030ee67e8abee0d2c4b17580574d1f
incca <591980606@qq.com>
2018-11-03 23:05:18 +0800

    feat: add loading view.

--- src/RecentView/index.tsx
+++ src/RecentView/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { getPasteShortcut } from '../utils';
 import ClipboardContainer from '../components/ClipboardContainer';
 import { getValue, setValue, STORAGE_KEY } from '../storage';
+import ReactLoading from 'react-loading';
 import './index.less';
 
 interface ClipboardData {
@@ -11,6 +12,7 @@ interface ClipboardData {
 
 interface State {
   clipboardData: ClipboardData;
+  isLoading: boolean;
 };
 
 interface Property {};
@@ -21,16 +23,36 @@ class RecentView extends React.Component {
       types: [],
       details: [],
     },
+    isLoading: true,
   }
   constructor(props: Property) {
     super(props);
     this.pasteEventHandler = this.pasteEventHandler.bind(this);
-    getValue(STORAGE_KEY.RECENT).then((value) => {
+    getValue(STORAGE_KEY.RECENT)
+    .then((value) => {
       if (value) {
         this.setState({
+          isLoading: false,
           clipboardData: value,
         });
+      } else {
+        this.setState({
+          isLoading: false,
+          clipboardData: {
+            types: [],
+            details: [],
+          },
+        });
       }
+    })
+    .catch(() => {
+      this.setState({
+        isLoading: false,
+        clipboardData: {
+          types: [],
+          details: [],
+        },
+      });
     });
   }
   componentDidMount() {
@@ -67,12 +89,16 @@ class RecentView extends React.Component {
     );
   }
   render() {
-    const {clipboardData} = this.state;
-    return (
-      clipboardData.types.length === 0
+    const { clipboardData, isLoading, } = this.state;
+    let view = null;
+    if (isLoading) {
+      view = <ReactLoading type={'spokes'} color={'#007cf9'} className="recent-loading" width="10%" height="10%" />;
+    } else {
+      view = clipboardData.types.length === 0
         ? <div className="paste-tip">{getPasteShortcut()} 粘贴数据</div>
-        : this.renderView()
-    );
+        : this.renderView();
+    }
+    return view;
   }
 };
 
